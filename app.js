f3ddf60a3a6f132858c14eb7b06ce225a1fada32
Jon Frederick <JonFred151@gmail.com>
2024-01-31 16:04:43 -0600

    Complete

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Johnny";
+let lastName;
+var age;
 // Exercise 2
 
+lastName = "Test"
+age = 26;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4 
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false
+let selectedSize = '"M"'
+//Exerdise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 465;
+let hasRead = true;
